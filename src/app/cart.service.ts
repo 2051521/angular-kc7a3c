69c53d08023691c2cdbe435177472d7655f1e0a9
Maxime Ménard <2051521@cegepoutaouais.qc.ca>
2022-08-30 08:29:22 -0400

    5

---
+++ src/app/cart.service.ts
@@ -0,0 +1,33 @@
+import { Product } from './products';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+  constructor(private http: HttpClient) {}
+
+  // The addToCart() method appends a product to an array of items
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  // The getItems() method collects the items users add to the cart and returns each item with its associated quantity
+  getItems() {
+    return this.items;
+  }
+
+  // The clearCart() method returns an empty array of items, which empties the cart
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+}
